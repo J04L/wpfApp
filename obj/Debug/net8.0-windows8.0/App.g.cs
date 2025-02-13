ede9159b9c4e2715861f752ca5615ea59d147704
Elías Bijloo Pardo <eliasbijloopardo@gmail.com>
2025-02-13 19:56:23 +0100

    vistas Rerservas

--- obj/Debug/net8.0-windows8.0/App.g.cs
+++ obj/Debug/net8.0-windows8.0/App.g.cs
@@ -47,7 +47,7 @@ namespace app_wpf {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace app_wpf {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public static void Main() {
             app_wpf.App app = new app_wpf.App();
             app.InitializeComponent();
--- obj/Debug/net8.0-windows8.0/App.g.i.cs
+++ obj/Debug/net8.0-windows8.0/App.g.i.cs
@@ -47,7 +47,7 @@ namespace app_wpf {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace app_wpf {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/app_wpf;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/app_wpf;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace app_wpf {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public static void Main() {
             app_wpf.App app = new app_wpf.App();
             app.InitializeComponent();
